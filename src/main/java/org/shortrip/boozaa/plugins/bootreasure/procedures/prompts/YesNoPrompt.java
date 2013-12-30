128a871aa69c405d250d33b58bdaf5ef5b276419
boozaa <boozaa@shortrip.org>
2013-12-30 16:13:58 +0100

    YesNoPrompt - add No possibility
    Signed-off-by: boozaa <boozaa@shortrip.org>

--- src/main/java/org/shortrip/boozaa/plugins/bootreasure/procedures/prompts/YesNoPrompt.java
+++ src/main/java/org/shortrip/boozaa/plugins/bootreasure/procedures/prompts/YesNoPrompt.java
@@ -13,7 +13,7 @@ public abstract class YesNoPrompt extends ValidatingPrompt {
 	@Override
 	protected boolean isInputValid(ConversationContext context, String in) {
 		// oui ou o
-		if( in.equalsIgnoreCase(Const.CMD_YES) || in.equalsIgnoreCase(Const.CMD_YES.substring(1, 2)) ){
+		if( ( in.equalsIgnoreCase(Const.CMD_YES) || in.equalsIgnoreCase(Const.CMD_YES.substring(1, 2)) ) || ( in.equalsIgnoreCase(Const.CMD_NO) || in.equalsIgnoreCase(Const.CMD_NO.substring(1, 2)) )  ){
 			return true;
 		}
     	return false;
