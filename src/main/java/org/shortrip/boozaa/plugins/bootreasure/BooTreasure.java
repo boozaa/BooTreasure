9554092cb03431b79211ac3bb9ae3fe75c5d84c2
boozaa <boozaa@shortrip.org>
2014-01-10 08:09:00 +0100

    CleanUp

--- src/main/java/org/shortrip/boozaa/plugins/bootreasure/BooTreasure.java
+++ src/main/java/org/shortrip/boozaa/plugins/bootreasure/BooTreasure.java
@@ -20,19 +20,10 @@ import lombok.Getter;
 
 public class BooTreasure  extends JavaPlugin{
 
-	
 	@Getter private static BooTreasure instance;
 	@Getter private static Managers managers;
-	/*
-	@Getter private static MyConfigs configManager;
-	@Getter private static MyCommands commandsManager;
-	@Getter private static MyCron cronManager;
-	@Getter private static MyCache cacheManager;
-	@Getter private static MyEvents eventsManager;
-	@Getter private static MyPermissions permissionsManager;
-	@Getter private static MyTreasuresManager treasuresManager;
-	@Getter private static MyDatabase databaseManager;
-	*/
+
+	
 	
 	@Override
 	public void onEnable() {
@@ -56,19 +47,6 @@ public class BooTreasure  extends JavaPlugin{
 			Log.warning("Fatal error occured during startup, the plugin must be disabled");
 			Log.severe("The fatal error will be stored in plugins/BooTreasure/errors.txt", e);
 		}
-			
-			/*
-			// Instanciate system
-			configManager 			= new MyConfigs(this);
-			commandsManager 		= new MyCommands(this);
-			cronManager 			= new MyCron(this);
-			cacheManager 			= new MyCache(this);
-			eventsManager			= new MyEvents(this);
-			permissionsManager 		= new MyPermissions(this);
-			treasuresManager 		= new MyTreasuresManager(this);
-			databaseManager			= new MyDatabase(this);
-			*/
-			
 				
 	}
 	
@@ -90,22 +68,8 @@ public class BooTreasure  extends JavaPlugin{
 	@Override
 	public void onDisable() {
 		
-		/*
-		if( configManager != null)
-			configManager.onDisable();
-		if( commandsManager != null)
-			commandsManager.onDisable();
-		if( cronManager != null)
-			cronManager.onDisable();
-		if( cacheManager != null)
-			cacheManager.onDisable();
-		if( eventsManager != null)
-			eventsManager.onDisable();
-		if( permissionsManager != null)
-			permissionsManager.onDisable();
-		if( treasuresManager != null)
-			treasuresManager.onDisable();
-		*/
+		// TODO: launch onDisable() on managers
+		
 	}
 	
 }
